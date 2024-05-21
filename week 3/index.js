19488ca2913caa2dc6828cffc87d3a24a7c96b84
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-21 12:53:47 +0530

    trying the ugly way of the user auth and input validation

--- week 3/index.js
+++ week 3/index.js
@@ -5,9 +5,50 @@ app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-app.get("/health-checkup", function(req, res) {
 
-    res.send("Your heart is Healthy");
-})
+
+// app.get("/health-checkup", function(req, res) {
+//     const kidneyId = req.query.kidneyId;
+//     const username = req.headers.username;
+//     const password = req.headers.password;
+
+//     if (username != "gaurav" || password != "pass") {
+//         res.status(403).json({
+//             msg: "User doesnt exit",
+//         })
+//         return;
+
+//         if (kidneyId != 1 || kidneyId != 2) {
+//             res.status(411).json({
+//                 msg: "wrong inputs",
+//             })
+//             return;
+//         }
+//     }
+
+
+//     res.send("Your heart is Healthy");
+// })
+
+
+app.get("/health-checkup", function(req, res) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
+
+    if (username != "Gaurav" || password != "pass") {
+        res.status(400).json({ "msg": "Somethings up with your inputs" })
+        return
+    }
+    if (kidneyId != 1 && kidneyId != 2) {
+        res.status(400).json({ "msg": "Somethings up with your inputs" })
+        return
+    }
+    res.json({
+        msg: "Your kidney is fine"
+    })
+
+});
+
 
 app.listen(3000);
